7521ed852e2805192a7c76ff24c555fb63d4f6fe
cxycyq <799774821@qq.com>
2020-12-16 11:00:45 +0800

    [UPDATE]

--- Myblog/front_end/pigblog-vue/vue.config.js
+++ Myblog/front_end/pigblog-vue/vue.config.js
@@ -1,26 +1,23 @@
 module.exports = {
 
     publicPath: '/',
-    // devServer Options don't belong into `configureWebpack`
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/vue-admin-web/'
+    //     : '/',
 
     devServer: {
         open: process.platform === 'darwin',
-
         host: '0.0.0.0',
-
         port: 8281,
-
         public: '172.25.173.198:8281',
-
         hot: true,
-
         https: false,
-
         hotOnly: false,
-
         disableHostCheck: true,
-
         proxy: null,
+    },
+
+    configureWebpack:{
 
     },
 
